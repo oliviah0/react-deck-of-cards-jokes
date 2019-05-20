c57ee087a287d2a5c6a3d5dbde4980acb338cf6a
Parco Tam <ptam1556@gmail.com>
2019-05-20 11:43:21 -0700

    completed dadjokes exercise

--- src/Joke.js
+++ src/Joke.js
@@ -1,12 +1,29 @@
 import React, {Component} from "react";
 
 class Joke extends Component {
+  constructor(props) {
+    super(props);
+		this.handleUpVote = this.handleUpVote.bind(this);
+		this.handleDownVote = this.handleDownVote.bind(this);
+  }
+
+  handleUpVote() {
+    this.props.handleUpVote(this.props.id);
+  }
+
+  handleDownVote() {
+    this.props.handleDownVote(this.props.id);
+  }  
 
   render() {
     return (
-      <div>
-        HELLO JOKE
-      </div>
+      <li>
+        {this.props.joke}
+        <br/>
+        <button onClick={this.handleUpVote}> Up </button>
+        <button onClick={this.handleDownVote}> Down </button>
+        <p>{this.props.score}</p>
+      </li>
     );
   }
 }
--- src/Jokes.js
+++ src/Jokes.js
@@ -1,19 +1,58 @@
-import React, {Component} from "react";
-import Joke from "./Joke";
+import React, { Component } from 'react';
+import Joke from './Joke';
+import axios from 'axios';
 
 class Jokes extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			jokes: []
+		};
+		this.handleUpVote = this.handleUpVote.bind(this);
+		this.handleDownVote = this.handleDownVote.bind(this);
+	}
 
-  render() {
-    return (
-      <div>
-        Hello JOKE(S)
-        <Joke />
-      </div>
-    );
-  }
+	async componentDidMount() {
+		let response = await axios.get(`https://icanhazdadjoke.com/search?limit=10`, {
+			headers: {
+				'Accept': 'application/json'
+			}
+		});
+		let responseJokes = response.data.results;
+		let jokes = responseJokes.map((joke) => ({ ...joke, score: 0 }));
+		this.setState({ jokes });
+	}
+
+	handleUpVote(id) {
+    this.setState(st => ({
+      jokes: st.jokes.map(joke => (joke.id === id ? {...joke, score: joke.score += 1} : joke))
+    }));
+  }  
+
+	handleDownVote(id) {
+    this.setState(st => ({
+      jokes: st.jokes.map(joke => (joke.id === id ? {...joke, score: joke.score -= 1} : joke))
+    }));
+	}
+
+	render() {
+		const { jokes } = this.state;
+		return (
+			<div>
+				Hello JOKE(S)
+				{jokes.map((joke) => (
+					<Joke
+						key={joke.id}
+						id={joke.id}
+						joke={joke.joke}
+						handleUpVote={this.handleUpVote}
+            handleDownVote={this.handleDownVote}
+            score={joke.score}
+					/>
+				))}
+			</div>
+		);
+	}
 }
 
 export default Jokes;
